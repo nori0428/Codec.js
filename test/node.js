873e387bfb7c4ba6534e420aed98a56595e5a14d
uupaa <uupaa.js@gmail.com>
2015-01-07 09:17:23 +0900

    tweak ignore setting

---
+++ test/node.js
@@ -0,0 +1,10 @@
+// Codec test
+
+require("../lib/Codec.js");
+require("../lib/Doubler.js");
+require("../lib/MessagePack.js");
+require("../lib/ZLib.js");
+require(".././test/wmtools.js");
+require("../release/Codec.n.min.js");
+require("./testcase.js");
+
