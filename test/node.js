9a74526bdd016837fda3a45594674ad8597abc87
uupaa <uupaa.js@gmail.com>
2015-01-07 09:22:54 +0900

    resurrect

--- test/node.js
+++ test/node.js
@@ -1,10 +1,11 @@
 // Codec test
 
+require("../node_modules/uupaa.hash.js/lib/Hash.js");
+require(".././test/wmtools.js");
 require("../lib/Codec.js");
 require("../lib/Doubler.js");
 require("../lib/MessagePack.js");
 require("../lib/ZLib.js");
-require(".././test/wmtools.js");
 require("../release/Codec.n.min.js");
 require("./testcase.js");
 
---
+++ test/worker.js
@@ -0,0 +1,25 @@
+// Codec test
+
+onmessage = function(event) {
+    self.TEST_DATA = event.data;
+    self.TEST_ERROR_MESSAGE = "";
+
+    if (!self.console) {
+        self.console = function() {};
+        self.console.log = function() {};
+        self.console.warn = function() {};
+        self.console.error = function() {};
+    }
+
+    importScripts("../node_modules/uupaa.hash.js/lib/Hash.js");
+    importScripts(".././test/wmtools.js");
+    importScripts("../lib/Codec.js");
+    importScripts("../lib/Doubler.js");
+    importScripts("../lib/MessagePack.js");
+    importScripts("../lib/ZLib.js");
+    importScripts("../release/Codec.w.min.js");
+    importScripts("./testcase.js");
+
+    self.postMessage({ TEST_ERROR_MESSAGE: self.TEST_ERROR_MESSAGE || "" });
+};
+
